160eac0721a5a08d6438c244fc4e1c1dfc9b2332
Talisson Barbosa <talissonbarbosa@gmail.com>
2023-05-17 21:39:06 -0300

    alterando token de autenticação

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {  
     const endpoint = `https://api.github.com/user`
-    const token = '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'
+    const token = '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'
 
     // Defina o seu Personal Access Token aqui
 
@@ -8,7 +8,6 @@ fetch(endpoint, { headers: { Authorization: `Token ${token}`}})
 
 .then(resposta => resposta.json() )
 .then(data => {
-    console.log(data)
     const avatar = data.avatar_url
     const login = data.login
     const seguidores = data.followers
@@ -16,9 +15,6 @@ fetch(endpoint, { headers: { Authorization: `Token ${token}`}})
     const repositorios = data.public_repos
     const link = data.html_url
     const name = data.name
-
-    console.log(name)
-    console.log(seguidores)
     $('#avatar').attr('src',avatar)
     $('#repositorios').text(repositorios)
     $('#seguidores').text(seguidores)
